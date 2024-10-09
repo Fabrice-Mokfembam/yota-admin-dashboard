1e92229d53c438c76b3798c297300711117c9c2b
Fabrice-Mokfembam <fabricemokfembam@gmail.com>
2024-10-08 18:37:04 -0700

    tried resolving login in

--- src/App.jsx
+++ src/App.jsx
@@ -192,8 +192,7 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element:
-        Object.keys(admin).length !== 0 ? <Layout /> : <Navigate to="/login" />,
+      element: admin.name ? <Layout /> : <Navigate to="/login" />,
       children: [
         { path: "/", element: <Home reviews={reviews} /> },
         { path: "/chats", element: <Chats /> },
